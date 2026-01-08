193604cc8b281f6cb08976faa77deff3a0f5f980
Ambar Bansal <ambarbansal2@gmail.com>
2026-01-08 23:36:13 +0530

    added code

---
+++ engine/tunnel_controller.go
@@ -0,0 +1,15 @@
+package engine
+
+import "Atanser/monitor"
+
+type TunnelController struct{}
+
+func (t *TunnelController) StartTunnel() string {
+	monitor.Core.TunnelState = true
+	return "Secure Tunnel Started"
+}
+
+func (t *TunnelController) StopTunnel() string {
+	monitor.Core.TunnelState = false
+	return "Secure Tunnel Stopped"
+}
